faa01bc150ea514538a9f204bfd8f2b475183cc4
Timothé Rios <trios.timothe@hotmail.fr>
2023-05-19 11:31:44 +0200

    Added rack positions through odbc

--- AR/source/ODBC.py
+++ AR/source/ODBC.py
@@ -8,19 +8,18 @@ uid="odbcuser"
 pwd="dcTrack$1"
 def Init(server,uid,pwd,database="raritan",driver="{PostgreSQL ANSI}",port="2235"):
     global cnxn
-    cnxn = pyodbc.connect(f'Driver={driver};Server={server};Port={port};Database={database};Uid={uid};Pwd={pwd};')
+    cnxn = pyodbc.connect(driver=driver,server=server,port=port,database=database,uid=uid,pwd=pwd)
     # Python 3.x
     cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
     cnxn.setencoding(encoding='utf-8')
 
-def test():
+def GetPosition(rackName=None,rackID=None):
     if "cnxn" not in globals():
         Init(server,uid,pwd,database,driver,port)
-
-    # Create a cursor from the connection
+    if (rackID is None and rackName is None):
+        raise Exception("Must give an id or a rack name")
     cursor = cnxn.cursor()
-    cursor.execute('SELECT * from "dcFloormapObjects" where "ItemID" = 3951 ')
-    row = cursor.fetchone()
-    while row:
-        print(row)
-        row = cursor.fetchone()
+    if (rackID is not None):
+        return cursor.execute(f'SELECT "MinY","MinX" from "dcFloormapObjects" where "ItemID" = {rackID}').fetchone()
+    return cursor.execute(f'SELECT "MinY","MinX" from "dcFloormapObjects" where "ItemName" =\'{rackName}\'').fetchone()
+    
