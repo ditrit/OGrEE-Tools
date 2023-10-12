26ec09d23f6113e0791073868c3cef84683a49b6
Pakoooooooooo <130537891+Pakoooooooooo@users.noreply.github.com>
2023-10-12 11:23:39 +0200

    Update NonSquareRooms.py

--- ToolBox/NonSquareRooms.py
+++ ToolBox/NonSquareRooms.py
@@ -2,6 +2,15 @@ import tkinter as tk
 from tkinter import filedialog
 
 def create_NSR(self):
+        cur = os.getcwd()
+        os.chdir(cur+"\\OGrEE-Tools\\NonSquareRooms\\setup")
+        import setup as s
+        os.chdir(cur)
+        path = cur+"\\OGrEE-Tools\\.venv\\Scripts\\activate"
+        subprocess.call(f"{path}", shell=True)
+        os.chdir(cur+"\\OGrEE-Tools\\NonSquareRooms")
+        import GenTiles as G
+        os.chdir(cur)
         frame = tk.Frame(self.root, width=200, height=100, bg="yellow")
         frame.pack_propagate(False)
         label = tk.Label(frame, text="Frame 1 Content", bg="yellow", fg="white")
